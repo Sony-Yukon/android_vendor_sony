46827963198035f259c007acc5cd804e380e3446
Ramón Hernández <rmnhg16@gmail.com>
2017-12-27 20:36:47 +0100

    Remove non-yukon devices' blobs

--- amami/amami-vendor.mk
+++
@@ -1,6 +0,0 @@
-PRODUCT_PLATFORM := rhine
-$(call inherit-product, vendor/qcom/prebuilt/qcom-vendor.mk)
-$(call inherit-product, vendor/sony/rhine/rhine-partial.mk)
-$(call inherit-product, vendor/sony/amami/amami-partial.mk)
-$(call inherit-product, vendor/sony/amami/amami-vendor-blobs.mk)
-$(call inherit-product, vendor/sony/rhine/rhine-vendor-blobs.mk)
--- tulip/tulip-vendor.mk
+++
@@ -1,4 +0,0 @@
-PRODUCT_PLATFORM := kanuti
-$(call inherit-product, vendor/qcom/prebuilt/qcom-vendor.mk)
-$(call inherit-product, vendor/sony/kanuti/kanuti-partial.mk)
-$(call inherit-product, $(LOCAL_PATH)/tulip-partial.mk)
