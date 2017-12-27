c6ea10ca79b7e36ec1cec18753e167b296189ad7
Ramón Hernández <rmnhg16@gmail.com>
2017-12-27 20:42:23 +0100

    Fix some paths

--- tianchi/tianchi-vendor.mk
+++ tianchi/tianchi-vendor.mk
@@ -1,4 +1,3 @@
 PRODUCT_PLATFORM := yukon
-$(call inherit-product, vendor/qcom/prebuilt/qcom-vendor.mk)
-$(call inherit-product, vendor/sony/yukon/yukon-partial.mk)
+$(call inherit-product, vendor/sony/yukon/yukon-vendor.mk)
 $(call inherit-product, $(LOCAL_PATH)/tianchi-partial.mk)
